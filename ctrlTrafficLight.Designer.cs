8710d1c303c20900ded11087f6f40dd24ca4dae2
Reda Hilal <reda0x04@gmail.com>
2025-03-07 21:53:29 +0400

    Initial commit

---
+++ ctrlTrafficLight.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Traffic_Light_Project
+{
+    partial class ctrlTrafficLight
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbLight = new System.Windows.Forms.PictureBox();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.LightTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbLight
+            // 
+            this.pbLight.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbLight.Image = global::Traffic_Light_Project.Properties.Resources.Red;
+            this.pbLight.Location = new System.Drawing.Point(0, 0);
+            this.pbLight.Name = "pbLight";
+            this.pbLight.Size = new System.Drawing.Size(72, 143);
+            this.pbLight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLight.TabIndex = 0;
+            this.pbLight.TabStop = false;
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblCountDown.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountDown.ForeColor = System.Drawing.Color.White;
+            this.lblCountDown.Location = new System.Drawing.Point(0, 146);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(72, 37);
+            this.lblCountDown.TabIndex = 1;
+            this.lblCountDown.Text = "0";
+            this.lblCountDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LightTimer
+            // 
+            this.LightTimer.Interval = 1000;
+            this.LightTimer.Tick += new System.EventHandler(this.LightTimer_Tick);
+            // 
+            // ctrlTrafficLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.lblCountDown);
+            this.Controls.Add(this.pbLight);
+            this.Name = "ctrlTrafficLight";
+            this.Size = new System.Drawing.Size(72, 183);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbLight;
+        private System.Windows.Forms.Label lblCountDown;
+        private System.Windows.Forms.Timer LightTimer;
+    }
+}
---
+++ ctrlTrafficLight.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traffic_Light_Project
+{
+    public partial class ctrlTrafficLight : UserControl
+    {
+        public enum enLight { Red = 0, Yellow = 1, Green = 2 };
+        
+        private enLight _CurrentLight = enLight.Red;
+        public enLight CurrentLight
+        {
+            get { return _CurrentLight; }
+        
+            set 
+            { 
+                _CurrentLight = value; 
+                
+                switch (value)
+                {
+                    case enLight.Red:
+                        pbLight.Image = Properties.Resources.Red;
+                        lblCountDown.ForeColor = Color.Red;
+                        break;
+                        
+                    case enLight.Yellow:
+                        pbLight.Image = Properties.Resources.Yellow;
+                        lblCountDown.ForeColor = Color.Yellow;
+                        break;
+                        
+                    case enLight.Green:
+                        pbLight.Image = Properties.Resources.Green;
+                        lblCountDown.ForeColor = Color.Green;
+                        break;
+
+                    default:
+                        pbLight.Image = Properties.Resources.Red;
+                        lblCountDown.ForeColor = Color.Red;
+                        break;
+
+                }
+
+            }
+        }
+
+        private int _CurrentCountDownValue = 0;
+
+        private int _RedLightTime = 10;
+        private int _YellowLightTime = 3;
+        private int _GreenLightTime = 10;
+        
+        public int RedLightTime { get { return _RedLightTime; } set { _RedLightTime = value; } }
+        public int YellowLightTime { get { return _YellowLightTime; } set { _YellowLightTime = value; } }
+        public int GreenLightTime { get { return _GreenLightTime; } set { _RedLightTime = value; } }
+
+        public class TrafficLightEventArgs : EventArgs
+        {
+            public enLight CurrentLight { get; }
+            public int LightDuration { get; }
+
+            public TrafficLightEventArgs(enLight currentLight, int lightDuration)
+            {
+                CurrentLight = currentLight;
+                LightDuration = lightDuration;
+            }
+        }
+
+        public event EventHandler<TrafficLightEventArgs> TrafficLightChanged;
+        public event EventHandler<TrafficLightEventArgs> RedLightOn;
+        public event EventHandler<TrafficLightEventArgs> RedLightOff;
+        public event EventHandler<TrafficLightEventArgs> YellowLightOn;
+        public event EventHandler<TrafficLightEventArgs> YellowLightOff;
+        public event EventHandler<TrafficLightEventArgs> GreenLightOn;
+        public event EventHandler<TrafficLightEventArgs> GreenLightOff;
+
+        protected virtual void RaiseTrafficLightChanged(TrafficLightEventArgs e)
+        {
+            TrafficLightChanged.Invoke(this, e);
+        }
+        protected virtual void RaiseRedLightOn(TrafficLightEventArgs e)
+        {
+            RedLightOn.Invoke(this, e);
+        }
+        protected virtual void RaiseRedLightOff(TrafficLightEventArgs e)
+        {
+            RedLightOff.Invoke(this, e);
+        }
+        protected virtual void RaiseYellowLightOn(TrafficLightEventArgs e)
+        {
+            YellowLightOn.Invoke(this, e);
+        }
+        protected virtual void RaiseYellowLightOff(TrafficLightEventArgs e)
+        {
+            YellowLightOff.Invoke(this, e);
+        }
+        protected virtual void RaiseGreenLightOn(TrafficLightEventArgs e)
+        {
+            GreenLightOn.Invoke(this, e);
+        }
+        protected virtual void RaiseGreenLightOff(TrafficLightEventArgs e)
+        {
+            GreenLightOff.Invoke(this, e);
+        }
+
+        public ctrlTrafficLight()
+        {
+            InitializeComponent();
+        }
+
+        public int GetCurrentTime()
+        {
+            switch (_CurrentLight)
+            {
+                case enLight.Red:
+                    return RedLightTime;
+                    
+                case enLight.Yellow:
+                    return YellowLightTime;
+                    
+                case enLight.Green:
+                    return GreenLightTime;
+
+                default:
+                    return RedLightTime;
+
+            }
+        }
+
+        public void Start()
+        {
+            _CurrentCountDownValue = GetCurrentTime();
+            LightTimer.Start();
+        }
+        public void Stop()
+        {
+            LightTimer.Stop();
+        }
+
+        private void LightTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
